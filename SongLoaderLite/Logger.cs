29e9e7547be6c32abf9c516aa1ab3375d93decfb
KennyTill <kenneth.g.till@gmail.com>
2018-11-26 01:02:22 -0800

    creation of static logger function to be used throughout the project

---
+++ SongLoaderLite/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace SongLoaderLite
+{
+    /// <summary>
+    /// Logger class for sending out messages to the --verbose console during the main application runtime
+    /// </summary>
+    static class Logger
+    {
+        /// <summary>
+        /// Basic static logging function, can be called from anywhere in code
+        /// </summary>
+        /// <param name="severity">Level of severity for display</param>
+        /// <param name="message">Message to be displayed in the log line</param>
+        public static void Log(Severity severity, string message)
+        {
+            Console.WriteLine("[Song Loader Lite][" + severity.ToString() + "] " + message);
+
+        }
+
+        public enum Severity
+        {
+            Info,
+            Warn,
+            Error,
+            Critical
+        }
+    }
+}
