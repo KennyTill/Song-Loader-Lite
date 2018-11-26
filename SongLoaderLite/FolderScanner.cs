a618a1a9cd72973bc23d03c395c94ec615bfbddb
KennyTill <kenneth.g.till@gmail.com>
2018-11-26 01:06:32 -0800

    Adding in folder scanner class

---
+++ SongLoaderLite/FolderScanner.cs
@@ -0,0 +1,9 @@
+﻿namespace SongLoaderLite
+{
+    /// <summary>
+    /// Folder scanning class, used for detecting songs info files for loading into the applicaiton.
+    /// </summary>
+    class FolderScanner
+    {
+    }
+}
