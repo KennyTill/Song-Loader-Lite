36bf0e327c7bd3f04d3304b9f2d6a3bd876bea1f
KennyTill <kenneth.g.till@gmail.com>
2018-11-26 01:08:44 -0800

    adding in placeholder song class with todo

---
+++ SongLoaderLite/Song.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongLoaderLite
+{
+    /// <summary>
+    /// Song model class that will hold all of the information needed for our custom song
+    /// </summary>
+    class Song
+    {
+        //TODO: add in song properties that will be used throughout the program
+    }
+}
