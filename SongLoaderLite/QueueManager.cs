16f16de0b7d38d6bc5650031548953426557a77e
KennyTill <kenneth.g.till@gmail.com>
2018-11-26 01:23:03 -0800

    adding in queue management template class, for future use with QueueSaber or compatible applications

---
+++ SongLoaderLite/QueueManager.cs
@@ -0,0 +1,10 @@
+﻿namespace SongLoaderLite
+{
+    /// <summary>
+    /// Management class for gathering queue information for multiplayer sessions
+    /// </summary>
+    class QueueManager
+    {
+        //TODO: add in queue management screen, JSON queue reader
+    }
+}
