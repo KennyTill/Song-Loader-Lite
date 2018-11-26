12c30ff6c815e05b509ec322662b75340316a72b
KennyTill <kenneth.g.till@gmail.com>
2018-11-26 01:31:07 -0800

    adding in basic testing for parent directory scan

--- SongLoaderLite/FolderScanner.cs
+++ SongLoaderLite/FolderScanner.cs
@@ -1,4 +1,7 @@
-﻿namespace SongLoaderLite
+﻿using System.IO;
+using System.Collections;
+
+namespace SongLoaderLite
 {
     /// <summary>
     /// Folder scanning class, used for detecting songs info files for loading into the applicaiton.
@@ -6,5 +9,19 @@
     class FolderScanner
     {
         //TODO: add in functions for scanning though the folders
+
+
+        /// <summary>
+        /// Scans for the info.json files in a given path, using the default beatsaver directory structure layout.
+        /// </summary>
+        /// <param name="SongPath">Path of the custom song directory to scan</param>
+        public void ScanForInfoFiles(string songPath)
+        {
+            foreach (string parentDirectory in Directory.GetDirectories(songPath))
+            {
+                Logger.Log(Logger.Severity.Info, parentDirectory);
+            }
+        }
+
     }
 }
