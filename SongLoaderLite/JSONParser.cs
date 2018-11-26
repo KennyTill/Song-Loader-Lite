2c577199af9efd9dfe9cb988d917e7eeba8f264e
KennyTill <kenneth.g.till@gmail.com>
2018-11-26 01:11:36 -0800

    adding in template JSON class for song parsing from info files

---
+++ SongLoaderLite/JSONParser.cs
@@ -0,0 +1,10 @@
+﻿namespace SongLoaderLite
+{
+    /// <summary>
+    /// JSON parsing class, used for reading song data and translating them to our Song model class
+    /// </summary>
+    class JSONParser
+    {
+        //TODO: folder scanner will hand off the information files to this class, to parse out information, then mangle them in to our song model
+    }
+}
